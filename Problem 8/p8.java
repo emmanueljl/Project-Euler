6e27abff6b028250bc949a9e75d2bfda99baac05
Emmanuel Lee <eman.j.lee@gmail.com>
2016-08-19 15:42:56 -0400

    Problem 8 solution - remember to use long ints!

--- Problem 8/p8.java
+++ Problem 8/p8.java
@@ -1,17 +1,13 @@
-import java.lang.Character;
-
-/* TODO: Current solution is incorrect. If by "adjacent", the problem means that the digits can be up/down/left/right-adjacent, not just left/right-adjacent, then this needs to be re-implemented as multiple BFS's of length 13. */
-
 class P8 {
 
     public static void main(String[] args) {
         final String num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
-        int left = 0, right = 13;
-        int maxProduct = 0, maxLeft = left, maxRight = right;
+        int left = 0, right = 13, maxLeft = left, maxRight = right;
+        long maxProduct = 0;
 
         while (right <= num.length()) {
-            int product = stringProduct(num.substring(left, right));
+            long product = stringProduct(num.substring(left, right));
             if (product > maxProduct) {
                 maxProduct = product;
                 maxLeft = left;
@@ -26,10 +22,11 @@ class P8 {
         System.out.println(num.substring(maxLeft, maxRight));
     }
 
-    static int stringProduct(final String num) {
-        int product = 1;
+    static long stringProduct(final String num) {
+        long product = 1;
         for (int i = 0; i < num.length(); i++) {
-            product *= Character.getNumericValue(num.charAt(i));
+            int digit = num.charAt(i) - '0'; // convert to int
+            product *= digit;
         }
         return product;
     }
